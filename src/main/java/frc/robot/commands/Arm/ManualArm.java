a9303c64d096405e985a831e3ff31a7e01607315
PossibilityJoshuaB <116594334+PossibilityJoshuaB@users.noreply.github.com>
2024-02-22 16:35:32 -0500

    Feed stuff + commented out double trigger feed

---
+++ src/main/java/frc/robot/commands/Arm/ManualArm.java
@@ -0,0 +1,36 @@
+package frc.robot.commands.Arm;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.Arm.Arm;
+
+public class ManualArm extends Command {
+  private final DoubleSupplier m_angleSupplier;
+  private final Arm m_arm;
+
+  public ManualArm(DoubleSupplier angleSupplier, Arm arm) {
+    m_angleSupplier = angleSupplier;
+    m_arm = arm;
+
+    this.addRequirements(m_arm);
+  }
+
+  @Override
+  public void execute() {
+    double armY = MathUtil.applyDeadband(this.m_angleSupplier.getAsDouble(), 0.1);
+    // double armY = this.m_angleSupplier.getAsDouble();
+
+    double armChange = (50 / 20) * armY;
+    this.m_arm.runSetpoint(new Rotation2d(
+      Units.degreesToRadians(armChange + m_arm.getSetpoint().getDegrees())
+    ));
+  }
+}
